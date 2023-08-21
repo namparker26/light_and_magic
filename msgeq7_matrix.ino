0ef7a51567459a6dfc6003c5229e37cdbea3edc8
Nam Nguyen <tug44395@temple.edu>
2023-08-22 00:23:31 +0700

    first working, controlling led matrix, using simple loop

--- msgeq7_matrix.ino
+++ msgeq7_matrix.ino
@@ -4,6 +4,7 @@
 #define msg7RESET 11
 #define msg7Strobe 12
 #define msg7DCout 0
+const int LEDpins[7] = {3,4,5,6,9,9,10}; // there are 5 LEDs and 7 freq bands. So, repeat LEDs
 
 #include "LedControl.h"
 #include <stdlib.h>
@@ -15,25 +16,26 @@ using namespace std;
 int DIN = 13;
 int CS = 8;
 int CLK = 2;
+LedControl lc=LedControl(DIN, CLK, CS,1); //assign pins for led control
+
 //Mario's Ideas
 //MAX7219 - using Led Control library to display all   leds one by one
+//define pins for led matrix control
 #define pushButton 2
 
-const int LEDpins[7] = {3,4,5,6,9,9,10}; // there are 5 LEDs and 7 freq bands. So, repeat LEDs
-LedControl lc=LedControl(DIN, CLK, CS,1); //define pins for led matrix control
 
 void setup() {
   Serial.begin(9600);
-   // initialize the digital pin as an output.
-   // Pin 13 has an LED connected on most Arduino boards:
+  //  initialize the digital pin as an output.
+  //  Pin 13 has an LED connected on most Arduino boards:
    for (int x=0; x<7; x++) {
       pinMode(LEDpins[x], OUTPUT); //defining the LEDpin of [x] to be OUTPUT, using in the loop below
    }
    pinMode(msg7RESET, OUTPUT);
    pinMode(msg7Strobe, OUTPUT);
 
-   pinMode(pushButton, INPUT); // never actually used in this example.
-   digitalWrite(pushButton, HIGH); // Enable internal pull-up
+  //  pinMode(pushButton, INPUT); // never actually used in this example.
+  //  digitalWrite(pushButton, HIGH); // Enable internal pull-up
 
   //led matrix set up
   lc.shutdown(0,false);
@@ -89,49 +91,53 @@ void undisplayColumn(int col) {
   // }
 }
 
-void displayLed(int PWMvalue, int x){
-  for(int i = 0; i < PWMvalue; i++){
+void displayLed(int ledMatrixValue, int x){
+  for(int i = 0; i < ledMatrixValue; i++){
     lc.setLed(0,x,i, true);
   }
 }
 
-void undisplayLed(int PWMvalue, int x){
-  for(int i = PWMvalue-1; i>=0; i--){
+void undisplayLed(int ledMatrixValue, int x){
+  for(int i = ledMatrixValue-1; i>=0; i--){
     lc.setLed(0,x,i, false);
   }
 }
 
+
 void loop() {
-   digitalWrite(msg7RESET, HIGH); // reset the MSGEQ7's counter
-   delay(5);
-   digitalWrite(msg7RESET, LOW);
+  int col_heights[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-   for (int x = 0; x < 7; x++){
-      digitalWrite(msg7Strobe, LOW); // output each DC value for each freq band
-      delayMicroseconds(35); // to allow the output to settle
-      int spectrumRead = analogRead(msg7DCout);
-      Serial.print("Raw ");
 
-      Serial.println(spectrumRead);
+  digitalWrite(msg7RESET, HIGH); // reset the MSGEQ7's counter
+  delay(5);
+  digitalWrite(msg7RESET, LOW);
 
-      int PWMvalue = map(spectrumRead, 0, 1024, 0, 255); // scale analogRead's value to Write's 255 max
-      if (PWMvalue < 50)
-         PWMvalue = PWMvalue / 2; // bit of a noise filter, so the LEDs turn off at low levels
+  for (int x = 0; x < 7; x++){
+    digitalWrite(msg7Strobe, LOW); // output each DC value for each freq band
+    delayMicroseconds(35); // to allow the output to settle
+    int spectrumRead = analogRead(msg7DCout);
 
-      int LEDvalue = map(spectrumRead, 0, 1024, 0, 8); // scale analogRead's value to Write's led matrix
+    int PWMvalue = map(spectrumRead, 0, 1024, 0, 255); // scale analogRead's value to Write's 255 max
+    if (PWMvalue < 50)
+        PWMvalue = PWMvalue / 2; // bit of a noise filter, so the LEDs turn off at low levels
 
-      // byte ledConfiguration[8];
-      // getRandomLedConfiguration(ledConfiguration, LEDvalue);
-      // for (int r = 0; r < 8; r++) {
-      //   lc.setRow(0, r, ledConfiguration[r]);
-      //   delay(20);
-      // }
+    int LEDvalue = map(PWMvalue, 0, 255, 0, 8); // scale analogRead's value to Write's led matrix
 
-      // for (int i = 7; i >= 0; i--) {
-      //   undisplayColumn(i);
-      // }
-      analogWrite(LEDpins[x], PWMvalue);
-      // Serial.println(PWMvalue);
-      digitalWrite(msg7Strobe, HIGH);
-   }
+    analogWrite(LEDpins[x], PWMvalue);
+    col_heights[x] = LEDvalue;
+    digitalWrite(msg7Strobe, HIGH);
+  
+    Serial.print(LEDvalue);
+    Serial.print(", ");
+  };
+  for (int x = 0; x <8; x++){
+    for (int i = 0; i < col_heights[x]; i++){
+      lc.setLed(0, x, i,true);
+    }
+    
+  };
+  delay(50);
+  lc.clearDisplay(0);
+
+   
 }
